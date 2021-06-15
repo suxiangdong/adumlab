783c87e1c67ab725913df93c566d154820689f93
cuining <cncc0824@gmail.com>
2021-06-15 08:49:55 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import ReactSlick from 'react-slick'
 import Layout from '../layouts/default'
 
@@ -24,80 +25,83 @@ export default function Home() {
       </ReactSlick>
       <section className="py-16 px-8 border-b border-fuchsia-600">
         <h2 className="text-4xl text-center mb-4">人气爆品</h2>
-        <div className="flex max-w-7xl mx-auto">
-          <a className="w-1/5" href="">
+        <div className="flex justify-center max-w-5xl mx-auto">
+          <Link href="/p/toaster">
+					<a className="w-1/5">
             <img className="hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/popular/0.jpg?20210429" />
-            <p className="">BALMUDA THE Cleaner</p>
-            <p className="">DCモーター扇風機</p>
+            <p className="">BALMUDA THE Toaster</p>
+            <p className="text-999 text-sm">スチームトースター</p>
           </a>
-          <a className="w-1/5" href="">
+					</Link>
+					<Link href="/p/cleaner">
+					<a className="w-1/5">
             <img className="hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/popular/9.jpg?20210429" />
             <p className="">BALMUDA THE Cleaner</p>
-            <p className="">DCモーター扇風機</p>
+            <p className="text-999 text-sm">ホバー式 クリーナー</p>
           </a>
-          <a className="w-1/5" href="">
+					</Link>
+					<Link href="/p/speaker">
+					<a className="w-1/5">
             <img className="hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/popular/7.jpg?20210429" />
-            <p className="">BALMUDA THE Cleaner</p>
-            <p className="">DCモーター扇風機</p>
+            <p className="">BALMUDA THE Speaker</p>
+            <p className="text-999 text-sm">ワイヤレススピーカー</p>
           </a>
-          <a className="w-1/5" href="">
+					</Link>
+					<Link href="/p/range">
+					<a className="w-1/5">
             <img className="hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/popular/10.jpg?20210429" />
-            <p className="">BALMUDA THE Cleaner</p>
-            <p className="">DCモーター扇風機</p>
+            <p className="">BALMUDA THE Range</p>
+            <p className="text-999 text-sm">オーブンレンジ</p>
           </a>
-          <a className="w-1/5" href="">
-            <img className="hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/popular/13.jpg?20210429" />
-            <p className="">BALMUDA THE Cleaner</p>
-            <p className="">DCモーター扇風機</p>
-          </a>
+					</Link>
+
         </div>
       </section>
-
       <section className="max-w-5xl mx-auto py-16 text-left grid grid-cols-3 grid-rows-3 gap-4">
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5@2x.jpg?20210429" alt="" />
           <h4 className="mb-2">バルミューダについて</h4>
           <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
         </a>
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
-          <h4 className="mb-2">バルミューダについて</h4>
-          <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/6@2x.jpg?20210530" alt="" />
+          <h4 className="mb-2">かけやすさを極めたクリーナー</h4>
+          <p className="text-sm text-gray-200">独自のホバーテクノロジーによる抜群の操作性と、優れた集塵性能で掃除時間を大幅に短縮します。</p>
         </a>
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
-          <h4 className="mb-2">バルミューダについて</h4>
-          <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/16@2x.jpg?20210530" alt="" />
+          <h4 className="mb-2">The GreenFan販売開始</h4>
+          <p className="text-sm text-gray-200">今年度の販売を開始しました。特別限定カラー「ホワイト×シャンパンゴールド」もご用意しています。</p>
         </a>
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
-          <h4 className="mb-2">バルミューダについて</h4>
-          <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/10@2x.jpg?20210530" alt="" />
+          <h4 className="mb-2">BALMUDA The Range限定カラー</h4>
+          <p className="text-sm text-gray-200">キッチンを楽しくするオーブンレンジに、オンラインストア限定カラー「ダークグレー」が登場。</p>
         </a>
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
-          <h4 className="mb-2">バルミューダについて</h4>
-          <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/19@2x.jpg?20210530" alt="" />
+          <h4 className="mb-2">新しい音楽体験を楽しむスピーカー</h4>
+          <p className="text-sm text-gray-200">BALMUDA The Speakerは、独自のサウンドと輝きでライブステージのような臨場感をつくり出します。</p>
         </a>
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
-          <h4 className="mb-2">バルミューダについて</h4>
-          <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/17@2x.jpg?20210530" alt="" />
+          <h4 className="mb-2">BALMUDA The Lantern限定カラー</h4>
+          <p className="text-sm text-gray-200">オンラインストア限定カラー「クラシックレッド」と「ネイビーブルー」が新登場。</p>
         </a>
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
-          <h4 className="mb-2">バルミューダについて</h4>
-          <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/0@2x.jpg?20210530" alt="" />
+          <h4 className="mb-2">ギフトラッピング</h4>
+          <p className="text-sm text-gray-200">大切な家族や友人へのプレゼントに。特別な思いを伝えるギフトラッピングをご用意しています。</p>
         </a>
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
-          <h4 className="mb-2">バルミューダについて</h4>
-          <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/12@2x.jpg?20210530" alt="" />
+          <h4 className="mb-2">BALMUDA 松屋銀座・阪急うめだ本店</h4>
+          <p className="text-sm text-gray-200">バルミューダ製品を体験できる特別なブランドショップです。</p>
         </a>
         <a href="">
-          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/5.jpg?20210429" alt="" />
-          <h4 className="mb-2">バルミューダについて</h4>
-          <p className="text-sm text-gray-200">バルミューダは2003年に東京で設立されたクリエイティブとテクノロジーの会社です。</p>
+          <img className="mb-6 hover:opacity-80" src="https://www.balmuda.com/jp/index/img/desktop/post/1@2x.jpg?20210530" alt="" />
+          <h4 className="mb-2">バルミューダのレシピ集</h4>
+          <p className="text-sm text-gray-200">トースター、ケトル、炊飯器、オーブンレンジあわせて100品を超えるレシピをご紹介。</p>
         </a>
       </section>
       <section className="py-8 bg-orange text-white">
---
+++ pages/toaster/toast.tsx
@@ -0,0 +1,100 @@
+import Link from 'next/link'
+import Layout from '../../layouts/default'
+
+export default function ProductDetail() {
+  const data = [
+    {
+      icon: 'https://www.balmuda.com/jp/toaster/img/svg/mode-toast.min.svgz?20200903',
+      backgroundImage: 'https://www.balmuda.com/jp/toaster/img/taste/desktop/section--01@2x.jpg?20200901',
+      title: '感動のトースト',
+      description: 'BALMUDA The Toasterで焼いたトーストは、表面がサックリとしたきつね色、中は水分と香りが十分に残ったまま熱々の仕上がり。鮮烈で、香ばしい小麦の匂いが印象的です。新鮮なバターや季節のジャム、ハチミツをたっぷりとかけてお召し上がりください。'
+    },
+    {
+      icon: 'https://www.balmuda.com/jp/toaster/img/svg/mode-cheese.min.svgz?20200903',
+      backgroundImage: 'https://www.balmuda.com/jp/toaster/img/taste/desktop/section--02@2x.jpg?20200901',
+      title: '究極のチーズトースト',
+      description: 'とけて、少しだけ焦げたチーズの素晴らしさに異論をとなえる人はいないでしょう。BALMUDA The Toasterはスチームの力でチーズの水分と風味をたっぷりと残したまま、細やかな温度制御で絶妙な焦げ目をつけます。シンプルなチーズトーストからピザトーストまで、感動の仕上がりをお約束します。',
+      align: 'right'
+    },
+    {
+      icon: 'https://www.balmuda.com/jp/toaster/img/svg/mode-croissant.min.svgz?20200903',
+      backgroundImage: 'https://www.balmuda.com/jp/toaster/img/taste/desktop/section--03@2x.jpg?20200901',
+      title: '幸せのクロワッサン',
+      description: 'トースターで温めると黒く焦げてしまいやすいクロワッサン。BALMUDA The Toasterでは、焦がさず表面はサクッと焼き上げ、窯から出したばかりの焼きたてのクロワッサンを再現します。香りたつバターと積層されたパイ生地。クロワッサン本来のおいしさを自宅でも楽しむことができます。'
+    },
+    {
+      icon: 'https://www.balmuda.com/jp/toaster/img/svg/mode-baguette.min.svgz?20200903',
+      backgroundImage: 'https://www.balmuda.com/jp/toaster/img/taste/desktop/section--04@2x.jpg?20200901',
+      title: '三ツ星のバゲット',
+      description: 'バゲットをトーストすると水分は飛び、表面も中身も硬くなってしまっていました。BALMUDA The Toasterは、スチームテクノロジーと細やかな温度制御で、パリッとした表面の歯ごたえと、中の柔らかさを同時に実現します。その香ばしさと食感はまさにレストランのバゲットです。',
+      align: 'right'
+    },
+    {
+      backgroundImage: 'https://www.balmuda.com/jp/toaster/img/taste/desktop/section--06@2x.jpg?20200901',
+      title: '冷凍したパンもおいしく',
+      description: 'BALMUDA The Toasterは、スチームテクノロジーと細やかな温度制御で、冷凍されたパンも簡単に、おいしく焼きあげます。いつもの焼き方に1分追加。たったそれだけで香ばしく、熱々に仕上がります。'
+    },
+    {
+      backgroundImage: 'https://www.balmuda.com/jp/toaster/img/taste/desktop/section--05@2x.jpg?20200901',
+      title: 'パンだけでなく料理にも',
+      description: 'スチームを使用せず、上下のヒーターだけをシンプルにつけるクラシックモード。170、200、230℃の3つの温度から選べます。お餅の焼き上げやグラタンなどはもちろん、庫内を一定の温度に保って焼き続けるので、クッキーなどの簡単なお菓子作りにも。BALMUDA The Toaster1台でお料理の幅がみるみる広がります。'
+    }
+  ]
+  return (
+    <Layout>
+      <div className="shadow-lg">
+        <div className="flex flex-col justify-between h-custom-90 max-w-5xl mx-auto pt-6 pb-3 px-3 text-left">
+          <Link href="/">
+            <a className="block w-72">
+              <img src="https://www.balmuda.com/_theme/img/svg/balmuda-the-toaster.min.svgz" alt="BALMUDA The Toaster" />
+            </a>
+          </Link>
+          <div className="text-xs">
+            <Link href="/">
+              <a className="mr-6">概要</a>
+            </Link>
+            <Link href="/">
+              <a className="text-green mr-6">特別なおいしさ</a>
+            </Link>
+            <Link href="/">
+              <a className="mr-6">トーストを科学する</a>
+            </Link>
+            <Link href="/">
+              <a className="mr-6">使い方</a>
+            </Link>
+            <Link href="/">
+              <a className="mr-6">レシピ</a>
+            </Link>
+            <Link href="/">
+              <a className="mr-6">ストーリー</a>
+            </Link>
+            <Link href="/">
+              <a className="mr-6">スペック</a>
+            </Link>
+            <Link href="/">
+              <a className="mr-6">アクセサリ</a>
+            </Link>
+            <Link href="/">
+              <a className="mr-6">販売店</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <section className="py-24 text-center">
+        <h2 className="text-4xl mb-8">味、香り、そして食感</h2>
+        <p className="text-lg mb-12 max-w-3xl px-3 leading-7 tracking-wide mx-auto">おいしさは味と香り、そして食感から生み出されます。BALMUDA The Toasterはスチームテクノロジーと細やかな温度制御で、さまざまなパンのおいしさを引き出します。温度が選べるクラシックモードでは、パン以外の食材もすばやく美味しく仕上げます。</p>
+      </section>
+      {data.map((item, index) => (
+        <section key={index} className="text-white relative">
+          <img src={item.backgroundImage} alt="" />
+          <div className={`flex flex-col justify-center absolute inset-0 m-auto transform ${item.align === 'right' ? '' : '-'}translate-x-2/4 max-w-md`}>
+            {item.icon && <img className="w-28 mb-7" src={item.icon} alt="" />}
+            <h2 className="text-4xl mb-8">{item.title}</h2>
+            <p className="text-lg mb-4 leading-8">{item.description}</p>
+          </div>
+        </section>
+      ))}
+    </Layout>
+  )
+}
